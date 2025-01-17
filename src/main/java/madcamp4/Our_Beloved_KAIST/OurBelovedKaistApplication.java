b0cb0ed9c3a9e18c75c5c95529f1d07470cb2bcc
doleebest <sj27419@gmail.com>
2025-01-17 20:27:32 +0900

    init

---
+++ src/main/java/madcamp4/Our_Beloved_KAIST/OurBelovedKaistApplication.java
@@ -0,0 +1,13 @@
+package madcamp4.Our_Beloved_KAIST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurBelovedKaistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurBelovedKaistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/madcamp4/Our_Beloved_KAIST/OurBelovedKaistApplicationTests.java
@@ -0,0 +1,13 @@
+package madcamp4.Our_Beloved_KAIST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurBelovedKaistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
