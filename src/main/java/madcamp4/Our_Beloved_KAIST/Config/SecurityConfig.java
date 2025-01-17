cdf4b8e6e36a38b4e34d3212053634c6ed6f8af7
doleebest <sj27419@gmail.com>
2025-01-17 22:28:05 +0900

    feat : temp login 2

--- src/main/java/madcamp4/Our_Beloved_KAIST/Config/SecurityConfig.java
+++ src/main/java/madcamp4/Our_Beloved_KAIST/Config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // React 개발 서버
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://our-beloved-kaist-web.web.app")); // React 개발 서버
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
